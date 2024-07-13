388670c3ae9c5bb7133eade488e3cade9272de8b
Henry Wu <wufuheng@gmail.com>
2024-07-12 18:05:56 -0700

    For rFID

---
+++ download.py
@@ -0,0 +1,16 @@
+import os
+import os.path as osp
+import torch
+
+setattr(torch.nn.Linear, 'reset_parameters', lambda self: None)     # disable default parameter init for faster speed
+setattr(torch.nn.LayerNorm, 'reset_parameters', lambda self: None)  # disable default parameter init for faster speed
+
+
+MODEL_DEPTH = 16    # TODO: =====> please specify MODEL_DEPTH <=====
+
+# download checkpoint
+hf_home = 'https://huggingface.co/FoundationVision/var/resolve/main'
+vae_ckpt, var_ckpt = 'vae_ch160v4096z32.pth', f'var_d{MODEL_DEPTH}.pth'
+if not osp.exists(vae_ckpt): os.system(f'wget {hf_home}/{vae_ckpt}')
+if not osp.exists(var_ckpt): os.system(f'wget {hf_home}/{var_ckpt}')
+
